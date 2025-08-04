2ed80e59d055b011eadae3745ab1c9988c977c26
imran <imranshaikh82639@gmail.com>
2025-08-04 17:45:39 +0530

    some small changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,14 +2,6 @@ export default function Footer() {
   return (
     <footer className="footer text-dark p-2 text-center">
       <p>Copyright 2025 Shaikh Imran, All Rights Reserved.</p>
-      <p>
-        Visitors:
-      
-<a href="https://www.hitwebcounter.com" target="_blank">
-<img src="https://hitwebcounter.com/counter/counter.php?page=21311206&style=0021&nbdigits=2&type=ip&initCount=0" title="Counter Widget" Alt="Visit counter For Websites"   border="0" />
-</a>                                    
-                                   
-      </p>
     </footer>
   );
 }
