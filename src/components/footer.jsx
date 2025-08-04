77bb5e00dfb7977a379386896aa0c62a81c49de9
imran <imranshaikh82639@gmail.com>
2025-08-04 19:01:42 +0530

    added counter

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,6 +2,12 @@ export default function Footer() {
   return (
     <footer className="footer text-dark p-2 text-center">
       <p>Copyright 2025 Shaikh Imran, All Rights Reserved.</p>
+
+      <p>
+        Visitors: {" "}
+      <img src="https://smallcounter.com/count.php?c_style=75&id=1754313787" border={0} alt="web counter" style={{ width: 15}}/>
+      </p>
+
     </footer>
   );
 }
